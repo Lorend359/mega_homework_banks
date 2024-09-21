a688fd530bc725fb4a6cfaf9f85f0ce63d879b8f
Svyatoslav <arend359@gmail.com>
2024-09-21 23:04:53 +0300

    commit first homework

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src.masks import get_mask_card_number, get_mask_account
 
-print(get_mask_card_number(input()))
-print(get_mask_account(input()))
+print(get_mask_card_number(str(input())))
+print(get_mask_account(str(input())))
