433c6f4ca6759e0a7a3306d3640b14380095e06d
Svyatoslav <arend359@gmail.com>
2024-09-26 23:18:38 +0300

    new module and func skeleton

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+def filter_by_state(list_of_dicts: [list], state: str = 'EXECUTED') -> [list]:
+    """ Фильтр словарей по переданному значению"""
+    pass
+
+
+def sort_by_date(list_of_dicts: [list], sort: bool = True) -> [list]:
+    """ Фильтр словарей по дате """
+    pass
