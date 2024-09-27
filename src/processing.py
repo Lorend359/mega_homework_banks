548ba9539fb8b1192d98f2f735b4f76914ba82cc
Svyatoslav <arend359@gmail.com>
2024-09-27 21:52:04 +0300

    создан и заполнен README, на основе предыдущих работ

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,11 @@
-def filter_by_state(list_of_dicts: [list], state: str = 'EXECUTED') -> [list]:
+from typing import Union
+
+
+def filter_by_state(list_of_dicts: Union[list[dict[str, int]]], state: str = 'EXECUTED') -> list[dict]:
     """ Фильтр словарей по переданному значению"""
     pass
 
 
-def sort_by_date(list_of_dicts: [list], sort: bool = True) -> [list]:
+def sort_by_date(list_of_dicts: Union[list[dict[str, int]]], sort: str = True) -> list[dict]:
     """ Фильтр словарей по дате """
     pass
