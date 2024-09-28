862668903803ed2cbb6d0805bab7058df5941aa2
Svyatoslav <arend359@gmail.com>
2024-09-28 11:18:18 +0300

    Change annotation, that mypy not causes errors

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 dict_lists = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -8,14 +6,14 @@ dict_lists = [
 ]
 
 
-def filter_by_state(list_of_dicts: Union[list[dict[str, int]]], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(list_of_dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Фильтр словарей по переданному значению"""
     filtered_dicts = [i for i in list_of_dicts if i.get("state") == state]
 
     return sorted(filtered_dicts, key=lambda x: x["state"])
 
 
-def sort_by_date(list_of_dicts: Union[list[dict[str, int]]], sort: bool = True) -> list[dict]:
+def sort_by_date(list_of_dicts: list[dict], sort: bool = True) -> list[dict]:
     """Фильтр словарей по дате"""
     for i in list_of_dicts:
         list_of_dicts.sort(key=lambda x: x["date"], reverse=sort)
--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,9 @@
+from typing import Any
+
 from .masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(card_info: str) -> str:
+def mask_account_card(card_info: str) -> Any:
     """
     Функция проверяет счёт или номер карты на входе,
     и маскирует соответствующее с использованием функций из masks.py
