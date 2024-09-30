787ced65db7449ba859f2c2f972ce86c4e2db09b
Svyatoslav <arend359@gmail.com>
2024-09-30 21:30:37 +0300

    masks first func was tested

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,12 @@
 def get_mask_card_number(card_type: str, card_number: str) -> str:
-    """Маскирует номер банковской карты"""
-    masked_card = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    """Маскирует номер банковской карты."""
+    if not card_number:
+        return f"{card_type} None** **** None"
+
+    if len(card_number) < 16:
+        return f"{card_type} {card_number} ** **** {card_number[-4:]}"  # Используем последние 4 цифры
+
+    masked_card = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"  # Маскируем правильным образом
     return f"{card_type} {masked_card}"
 
 
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,21 +1,30 @@
 import pytest
-
-from src.masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_card_number
 
 @pytest.fixture
 def card_data():
     return [
-        ("Visa", "1234567812345678", "Visa 1234 56** **** 5678"),
-        ("MasterCard", "8765432187654321", "MasterCard 8765 43** **** 4321"),
-        ("American Express", "378282246310005", "American Express 3782 82** **** 0005"),
+        ("Visa Platinum", "7000792289606361"),
+        ("Maestro", "1596837868705199"),
+        ("MasterCard", "7158300734726758"),
+        ("Visa Classic", "6831982476737658"),
+        ("Visa Platinum", "8990922113665229"),
+        ("Visa Gold", "5999414228426353"),
     ]
 
-
-@pytest.mark.parametrize("card_type, card_number, expected_output", [
-    ("Visa", "1234567812345678", "Visa 1234 56** **** 5678"),
-    ("MasterCard", "8765432187654321", "MasterCard 8765 43** **** 4321"),
-    ("American Express", "378282246310005", "American Express 3782 82** **** 0005"),
+@pytest.mark.parametrize("card_type, card_number, expected", [
+    ("Visa Platinum", "7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+    ("Maestro", "1596837868705199", "Maestro 1596 83** **** 5199"),
+    ("MasterCard", "7158300734726758", "MasterCard 7158 30** **** 6758"),
+    ("Visa Classic", "6831982476736758", "Visa Classic 6831 98** **** 6758"),
+    ("Visa Platinum", "8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+    ("Visa Gold", "5999414228426353", "Visa Gold 5999 41** **** 6353"),
 ])
-def test_get_mask_card_number(card_data, card_type, card_number, expected_output):
-    result = get_mask_card_number(card_type, card_number)
-    assert result == expected_output
+def test_get_mask_card_number(card_type, card_number, expected):
+    assert get_mask_card_number(card_type, card_number) == expected
+
+def test_get_mask_card_number_empty():
+    assert get_mask_card_number("Visa", "") == "Visa None** **** None"
+
+def test_get_mask_card_number_short():
+    assert get_mask_card_number("Test Card", "1234") == "Test Card 1234 ** **** 1234"
