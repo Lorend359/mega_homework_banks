0acc5246b4df96a0b93a18115455789408c50c38
Svyatoslav <arend359@gmail.com>
2024-10-01 00:41:39 +0300

    update readme

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
 dict_lists = [
@@ -7,51 +8,65 @@ dict_lists = [
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
-#тесты для функции filter_by_state
+
+
+# тесты для функции filter_by_state
 @pytest.fixture
 def sample_data():
     return dict_lists
 
 
-@pytest.mark.parametrize("state, expected", [
-    ("EXECUTED", [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    ]),
-    ("CANCELED", [
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]),
-    ("NOT_EXECUTED", []),
-])
+@pytest.mark.parametrize(
+    "state, expected",
+    [
+        (
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+        ("NOT_EXECUTED", []),
+    ],
+)
 def test_filter_by_state(sample_data, state, expected):
     result = filter_by_state(sample_data, state)
     assert result == expected
 
-#тесты для функции sort_by_date
-@pytest.mark.parametrize("input_data, sort_order, expected", [
-    (
-        dict_lists,
-        True,  # Сортировка по возрастанию
-        [
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        ]
-    ),
-    (
-        dict_lists,
-        False,  # Сортировка по убыванию
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        ]
-    ),
-])
+
+# тесты для функции sort_by_date
+@pytest.mark.parametrize(
+    "input_data, sort_order, expected",
+    [
+        (
+            dict_lists,
+            True,
+            [
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+        ),
+        (
+            dict_lists,
+            False,
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+    ],
+)
 def test_sort_by_date(sample_data, input_data, sort_order, expected):
     result = sort_by_date(input_data, sort_order)
     assert result == expected
-
