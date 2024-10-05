fa30edc8fa08374fff0d0ef1bf33c5e5991229db
Svyatoslav <arend359@gmail.com>
2024-10-05 22:43:06 +0300

    func and tests transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -4,3 +4,15 @@ def filter_by_currency(transactions, currency_code):
         if transaction.get('operationAmount', {}).get('currency', {}).get('code') == currency_code:
             yield transaction
 
+
+def transaction_descriptions(transactions):
+    """
+    Генератор, который возвращает описание каждой транзакции по очереди.
+    """
+    for transaction in transactions:
+        yield transaction.get('description', 'Нет описания')
+
+
+def card_number_generator(start, end):
+    ''' Генератор номеров банковских карт'''
+    pass
