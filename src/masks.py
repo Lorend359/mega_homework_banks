39488bd1bc5987b9a6732c81ea23c141ba7e13e4
Svyatoslav <arend359@gmail.com>
2024-10-22 21:17:51 +0300

    Logging for utils

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,16 @@
 import logging
 import os
 
-log_dir = 'logs'
+log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 
-logger = logging.getLogger('masks')
+logger = logging.getLogger("masks")
 logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler(os.path.join(log_dir, 'masks.log'), mode='w', encoding='utf-8')
+file_handler = logging.FileHandler(os.path.join(log_dir, "masks.log"), mode="w", encoding="utf-8")
 file_handler.setLevel(logging.DEBUG)
 
-file_formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+file_formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 
 logger.addHandler(file_handler)
@@ -41,12 +41,13 @@ def get_mask_account(account_type: str, account_number: str) -> str:
     logger.info(f"Замаскированный номер счёта для типа {account_type}: {masked_account}.")
     return f"{account_type} {masked_account}"
 
-#Вызов для проверки правильных данных
+
+# Вызов для проверки правильных данных
 get_mask_card_number("Visa", "1234567812345678")
 get_mask_account("Bank Account", "123456789012345")
 
-#Вызов ошибки
+# Вызов ошибки
 try:
-    get_mask_account("Bank Account", "not_a_number")  # Ошибка
+    get_mask_account("Bank Account", "not_a_number")
 except ValueError:
     pass
